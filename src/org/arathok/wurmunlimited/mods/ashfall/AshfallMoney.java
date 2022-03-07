b6c0edbfc8518a92340084b5ea3807704a92f9a1
Arathok <36503011+Arathok@users.noreply.github.com>
2022-03-07 22:16:13 +0100

    Money update

--- src/org/arathok/wurmunlimited/mods/ashfall/AshfallMoney.java
+++ src/org/arathok/wurmunlimited/mods/ashfall/AshfallMoney.java
@@ -5,6 +5,7 @@ import com.wurmonline.server.players.Player;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
 
 public class AshfallMoney {
     static HashMap<Player,Long> nextSalary = new HashMap<>();
@@ -16,25 +17,34 @@ public class AshfallMoney {
         time = System.currentTimeMillis();
         if (time > nextPoll) {
             Player[] players = Players.getInstance().getPlayers();
-            if (players!=null||players.length>0)
-            for (Player onePlayer : players) {
+            if (players != null || players.length > 0) {
 
-                if (!onePlayer.isLoggedOut() && onePlayer.isActiveInChat() && (nextSalary.containsKey(onePlayer) || nextSalary.get(onePlayer) < time)) {
-
-
-                    try {
-                        onePlayer.addMoney(500);
-                        onePlayer.getCommunicator().sendSafeServerMessage("Thank you for Playing on Ashfall! The Wesirs send you their regards in hoping you will restore this world to its former glory. You are bestowed a salary of 5 copper coins!");
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                for (Player onePlayer : players) {
+                    if (!nextSalary.containsKey(onePlayer)) {
+                        nextSalary.put(onePlayer, time + 120000);
+                        Ashfall.logger.log(Level.INFO,"Added Player "+onePlayer.getName()+" to the list!");
                     }
-                    nextSalary.replace(onePlayer, time + 86400000);
+                    else
+
+                    if (!onePlayer.isLoggedOut() && onePlayer.isActiveInChat() &&  nextSalary.get(onePlayer) < time) {
+
+
+                        try {
+                            onePlayer.addMoney(500);
+                            onePlayer.getCommunicator().sendSafeServerMessage("Thank you for Playing on Ashfall! The Wesirs send you their regards in hoping you will restore this world to its former glory. You are bestowed a salary of 5 copper coins!");
+                            Ashfall.logger.log(Level.INFO,"Payed out player "+onePlayer.getName()+ "their daily salary");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        nextSalary.replace(onePlayer, time + 86400000);
+                    }
+
+
                 }
 
 
             }
             nextPoll = time + 120000;
-
         }
     }
 
